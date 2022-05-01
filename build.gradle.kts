86b2d51cafa9a60864b5b50c4fc8322d6086d90a
mnajborowski <michal.najborowski@gmail.com>
2022-05-01 23:58:24 +0200

    add protobuf and grpc dependencies and set compiler task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+
+val protobufVersion = "3.20.1"
+val grpcVersion = "1.46.0"
+val grpcKotlinVersion = "1.2.1"
 
 plugins {
+    id("com.google.protobuf") version "0.8.18"
+
     kotlin("jvm") version "1.6.20"
     application
 }
@@ -12,7 +19,13 @@ repositories {
     mavenCentral()
 }
 
-
+dependencies {
+    implementation("io.grpc:grpc-netty:$grpcVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
@@ -20,4 +33,31 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+protobuf {
+    generatedFilesBaseDir = "src/main/kotlin"
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+            ofSourceSet("main")
+        }
+    }
 }
